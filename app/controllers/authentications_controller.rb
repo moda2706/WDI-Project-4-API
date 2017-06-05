f8e7be44b8d5f6fba00e8a7e77e7393c0d47e800
moda2706 <monju.miah27@gmail.com>
2017-06-05 11:05:45 +0100

    login bcrypt authentication

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -9,6 +9,12 @@ class AuthenticationsController < ApplicationController
   end
 
   def login
+    user = User.find_by_email(params[ :email])
+    if user && user.authenticate(params[ :password])
+      render json: user, status: :ok
+    else
+      render json: { errors: ["Invalid login credentials."]}, status: 401
+    end
   end
 
   private
