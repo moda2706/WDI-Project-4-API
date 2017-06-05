1b618073cd082fad850b7f792a5a12b674bb5e40
moda2706 <monju.miah27@gmail.com>
2017-06-05 10:57:17 +0100

    registration authentication

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,19 @@
+class AuthenticationsController < ApplicationController
+  def register
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :ok
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+  end
+
+  private
+
+    def user_params
+      params.permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602111100) do
+ActiveRecord::Schema.define(version: 20170605092008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "first_name"
     t.string "last_name"
-    t.string "username"
-    t.string "password_digest"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
