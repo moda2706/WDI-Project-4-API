48c74b5aecda5f0e01991a3309ab8fbe642dbd4e
Sophie2525 <blu.heart87@gmail.com>
2017-06-06 14:40:41 +0100

    starting user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,108 @@ user1 = User.create!(
 username: "Sophie",
 first_name: "Sophie",
 last_name: "Downey",
-email: "sophie@sophie.come",
+email: "sophie@sophie.com",
 password: "password",
-image: "")
+image: "http://canberravet.com.au/wp-content/uploads/2017/03/Rabbits.jpg",
+twitter: "",
+facebook: ""
+)
+
+user2 = User.create!(
+username: "Sophie2",
+first_name: "Sophie2",
+last_name: "Downey2",
+email: "sophie2@sophie2.com",
+password: "password",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZhN6s6kuVfcyBf2OOZcsCFg3z8WRX_4LWPaxdJbiii49G1KTa_g",
+twitter: "",
+facebook: ""
+)
+
+user3 = User.create!(
+username: "Sophie3",
+first_name: "Sophie3",
+last_name: "Downey3",
+email: "sophie3@sophie3.com",
+password: "password",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdsD6tg7yEzZggd6b73ODlbw6iUPyzMQ7g28ADMZlAMQP05lRH",
+twitter: "",
+facebook: ""
+)
+
+user4 = User.create!(
+username: "Sophie"4,
+first_name: "Sophie4",
+last_name: "Downey4",
+email: "sophie4@sophie4.com",
+password: "password",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXkB4-oWpEOZzv3jhW66pNY-50_X_u5y-Zg5fOjxaOkCtSbYc1",
+twitter: "",
+facebook: ""
+)
+
+user5 = User.create!(
+username: "Sophie5",
+first_name: "Sophie5",
+last_name: "Downey5",
+email: "sophie5@sophie5.com",
+password: "password",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQioJsI-6fnI5zpuGfMQjHe7qLfQnvGpTJBgKMZyGgssOAMydvysg",
+twitter: "",
+facebook: ""
+)
+
+user6 = User.create!(
+username: "Sophie6",
+first_name: "Sophie6",
+last_name: "Downey6",
+email: "sophie6@sophie6.com",
+password: "password",
+image: "http://i3.mirror.co.uk/incoming/article854307.ece/ALTERNATES/s615/Rafa-Benitez-Steven-Gerrard-Champions-League-Istanbul.jpg",
+twitter: "",
+facebook: ""
+)
+
+user7 = User.create!(
+username: "Sophie7",
+first_name: "Sophie7",
+last_name: "Downey7",
+email: "sophie7@sophie7.com",
+password: "password",
+image: "http://i.dailymail.co.uk/i/pix/2012/11/22/article-2236795-0283B6420000044D-245_634x301.jpg",
+twitter: "",
+facebook: ""
+)
+
+user8 = User.create!(
+username: "Sophie8",
+first_name: "Sophie8",
+last_name: "Downey8",
+email: "sophie8@sophie8.com",
+password: "password",
+image: "http://poster.keepcalmandposters.com/59536.png",
+twitter: "",
+facebook: ""
+)
+
+user9 = User.create!(
+username: "Sophie9",
+first_name: "Sophie9",
+last_name: "Downey9",
+email: "sophie9@sophie9.com",
+password: "password",
+image: "https://beckrow.suffolk.sch.uk/suffolk/primary/beckrow/arenas/websitecontent/web/rabbits-001.jpg?width=1920&height=1080&scale=LIMIT_MAXSIZE",
+twitter: "",
+facebook: ""
+)
+
+user10 = User.create!(
+username: "Sophie10",
+first_name: "Sophie10",
+last_name: "Downey10",
+email: "sophie10@sophie10.com",
+password: "password",
+image: "http://data.whicdn.com/images/17631250/379441_272549802789135_100001023520658_843245_785418404_n_large.jpg",
+twitter: "",
+facebook: ""
+)
