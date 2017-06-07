d3da57b2fd4aaf286ad6c919320a3da0a692067a
Sophie2525 <blu.heart87@gmail.com>
2017-06-07 13:41:55 +0100

    fixed scope error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :stops
-  resources :trips
-  resources :countries
+
   scope :api do
+    resources :stops
+    resources :trips
+    resources :countries
     resources :users
     post "/register", to: "authentications#register"
     post "/login", to: "authentications#login"
